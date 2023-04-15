6454ed295f77b8977af5a712d6e92cb72d3e3619
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-04-15 12:16:20 -0300

    testando_valores
    testando valores na função.

--- ENSAIO_LIVRO/qualificador_const.c
+++ ENSAIO_LIVRO/qualificador_const.c
@@ -3,12 +3,13 @@
 void sp_to_dash(const char *str);
 
 void main(void){	
-	sp_to_dash("isto é um teste");
+	sp_to_dash("isto é um teste\n");
+	sp_to_dash("meu teste agora");
 }
 
 void sp_to_dash(const char *str){
 	while(*str){
-		if(*str==' ') printf("%c", '_');
+		if(*str==' ') printf("%c", '-');
 		else printf("%c", *str);
 		str++;
 	}
