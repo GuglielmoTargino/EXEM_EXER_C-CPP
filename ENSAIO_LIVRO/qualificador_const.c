8c1b555aef289e9188d02787989d6e7d524137ad
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-04-15 12:13:11 -0300

    trocando_valores
    trocando valores para entendimento.

--- ENSAIO_LIVRO/qualificador_const.c
+++ ENSAIO_LIVRO/qualificador_const.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
 void sp_to_dash(const char *str);
-void main(void){
-	
+
+void main(void){	
 	sp_to_dash("isto é um teste");
+}
+
+void sp_to_dash(const char *str){
+	while(*str){
+		if(*str==' ') printf("%c", '_');
+		else printf("%c", *str);
+		str++;
+	}
 }
 
 
