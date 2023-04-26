4be66c38e0ed365af287ba2bffdf849aa0b9a46e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-04-26 17:31:56 -0300

    adicao_comentario
    Adicionado comentários apenas.

--- ENSAIO_LIVRO/ponteiro_endereco.c
+++ ENSAIO_LIVRO/ponteiro_endereco.c
@@ -1,14 +1,15 @@
 # include<stdio.h>
 
 void main(void){
+	
 	int target, source;
-	int *m;
+	int *m;/* forma de declarar uma variável como ponteiro*/
 	
-	target=8;
-	m=&target;
-	source=*m;
+	target=8; 
+	m=&target; /* m recebe o endereço de target*/
+	source=*m; /* source recebe o valor registrado no enderço passado em m*/
 	
-	printf("o enderço de target é %d",m);
+	printf("o endereço de target é %d\n",m);
 	printf("o valor de target é %d", target);	
 	
 }
