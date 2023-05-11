33bb9cb6eb4ae41b41ec628632ddb26b8935c8b0
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-05-11 19:38:53 -0300

    finalizado_exer_testesenha
    Finalizado o exercício do teste com senha.

--- ENSAIO_LIVRO/teste_senha.c
+++ ENSAIO_LIVRO/teste_senha.c
@@ -7,11 +7,13 @@ void main(void){
 	char str[20];
 	int x;
 	for (x=0; x<3 && strcmp(str,"senha");x++){
-		printf("Digite a senha por favor_");
+		printf("Digite a senha por favor. tentativa (%d)_\n",x+1);
 		gets(str);
 		
-	if (x==3) return;
-	printf("%d",x);
+		if (strcmp(str,"senha")==0) {
+			printf("Sucesso.\n");
+		}
+		   
 	
 	}
 }
