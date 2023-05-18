24a97c65df66db5b47dc22b463e5e12d830dbaf4
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-05-18 19:45:40 -0300

    fim_exer_while_strcpy().
    Entendido o exercício do comando while.

---
+++ ENSAIO_LIVRO/while_2.c
@@ -0,0 +1,29 @@
+# include<stdio.h>
+# include<string.h>
+
+void pad(char *s, int length);
+
+void main(void){
+	
+	char str[80];
+	
+	strcpy(str,"teste agora");
+	pad(str,40);
+	printf("%d ",strlen(str)); /* imprime o comprimento de <str>*/
+	printf("%s",str);
+			
+}
+/* acrescenta espaços aqui*/
+void pad(char *s, int length){/* <*s> recebe str,  e <length> recebe 40*/
+	int l;
+	l=strlen(s); /* acha o comprimrnto aqui*/
+
+	while(l<length){
+		s[l]='g'; /* insere espaço, nesse caso g*/
+		l++;
+
+		
+	}
+	s[l]='\0';
+	
+}
