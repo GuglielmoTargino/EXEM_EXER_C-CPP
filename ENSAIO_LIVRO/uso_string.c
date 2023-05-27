c13185452345188da70bf656a46a119e243435f4
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-05-26 23:53:39 -0300

    teste de compilação
    Feito teste de compilação apenas

--- ENSAIO_LIVRO/uso_string.c
+++ ENSAIO_LIVRO/uso_string.c
@@ -2,17 +2,17 @@
 #include<string.h>
 void main(void){
 	char s1[20],s2[20];
-	printf("digite uma string");
+	printf("digite uma string\n");
 	gets(s1);
-	printf("digite outra string");
+	printf("digite outra string\n");
 	gets(s2);
 	printf("comprimento de s1=%d, comprimento s2=%d\n",strlen(s1),strlen(s2));
 	if(!strcmp(s1,s2)) printf("as strings são iguais");
 	strcat(s1,s2);
 	printf("%s\n",s1);
-	printf("%s2\n",s2);
-	strcpy(s1,"isso é um teste.\n");
+	printf("%s\n",s2);
+	strcpy(s1,"isso é um teste de s1.\n");
 	printf(s1);
-	if(strchr("alo",'o')) printf("o está em alo\n");
-	if(strstr("olá aqui","olá")) printf("ola encontrado");	
+	if(strchr("alo",'m')) printf("o está em alo\n");
+	if(strstr("olá aqui","olá")) printf("ola encontrado em olá aqui");	
 }
