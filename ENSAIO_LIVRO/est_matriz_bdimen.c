7c0c1805cb73cb6f9f2a81acf6b54d7dc167a6ea
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-05-31 17:15:19 -0300

    est_matriz_bdimen
    Exercício com estudo de matriz

---
+++ ENSAIO_LIVRO/est_matriz_bdimen.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main(void){
+	int t,i, num[3][4];
+	
+	/*esse laço para escrever na matriz */
+	for(t=0;t<3;++t){
+		for(i=0;i<4;++i){
+			num[t][i]=(t*4)+i+1;
+			
+			
+		
+			
+		}
+	}
+		/* esse laço para imprimir*/
+	for(t=0;t<3;++t){
+		for(i=0;t<4;++i){
+			printf("%3d",num[t][i]);
+			printf("\n");
+		}
+	}
+}
