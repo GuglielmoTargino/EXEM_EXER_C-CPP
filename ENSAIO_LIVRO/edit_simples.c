7899e94b549a4f8db106ca4f9a089d44397e5193
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-06-01 19:32:25 -0300

    edit_simples
    Editor de texto simples

---
+++ ENSAIO_LIVRO/edit_simples.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define max 100
+#define len 80
+
+char tex [max][len];
+/* iditor de texto simples*/
+
+void main (void){
+	register int t,i,j;
+	printf("digite uma linha vazia para sair.\n");
+	
+	for(t=0;t<max;t++){
+		printf("%d",t);
+		
+	}
+}
