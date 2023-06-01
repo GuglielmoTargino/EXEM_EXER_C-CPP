c279d85a402d4cde1a50404210dab864ad6c8add
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-06-01 20:02:03 -0300

    ok_editor
    final editor simples

--- ENSAIO_LIVRO/edit_simples.c
+++ ENSAIO_LIVRO/edit_simples.c
@@ -2,15 +2,25 @@
 #define max 100
 #define len 80
 
-char tex [max][len];
+
+
+char text [max][len];
 /* iditor de texto simples*/
 
 void main (void){
-	register int t,i,j;
+	register int t,i,j=0;
 	printf("digite uma linha vazia para sair.\n");
 	
 	for(t=0;t<max;t++){
 		printf("%d",t);
+		gets(text[t]);
+		if(!*text[t]) break; /* sai se a linha for vazia*/
 		
+	}
+	
+	for(i=0;i<t;i++){
+		for(j=0;text[i][j];j++) putchar(text[i][j]);
+	
+		putchar('\n');
 	}
 }
