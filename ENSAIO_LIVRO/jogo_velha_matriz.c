cce9f415248e359fd9421fc07cb7af9311c32145
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-06-05 22:03:11 -0300

    ok_jogo_velha_matriz
    finalizado o jogo da velha com matrix

--- ENSAIO_LIVRO/jogo_velha_matriz.c
+++ ENSAIO_LIVRO/jogo_velha_matriz.c
@@ -17,13 +17,13 @@ void main(void){
 	printf("Você estará jogando comtra o computador,\n");
 	
 	done=' ';
-	init_matrx();
+	init_matrix();
 	do{
 		disp_matrix();
-	get_player_move();
-	done=check(); /* verifica se há vencedor*/
-	if(done!=' ') break; /* vencedor */
-	done=check(); /* verifica se há vencedor*/
+		get_player_move();
+		done=check(); /* verifica se há vencedor*/
+		if(done!=' ') break; /* vencedor */
+		done=check(); /* verifica se há vencedor*/
 	
 	
 	} while ( done==' ');
@@ -49,6 +49,56 @@ void get_player_move(void){
 	scanf("%d%d", &x, &y);
 	x--;
 	y--;
+	
+	if(matrix[x][y]!=' '){
+		printf("posição inválida. tente de nevo\n");
+		get_player_move();
+		
+	}else matrix[x][y]='x';
+	
 }
+/* obtem uma jogada do computador*/
+void get_computador_mover(void){
+	int i,j;
+	for (i=0; i<3; i++){
+		for(j=0;j<3;j++){
+			if (matrix[i][j]==' ') break;
+		if(matrix[i][j]==' ') break;		
+			
+		}
+	}
+	if(i*j==9){
+		printf("empate\n");
+		exit (0);
+	}else matrix[i][j]='0';
+}
+/*mostra a matrix na tela*/
+void disp_matrix(void){
+	int t=0;
+	for(t=0;t<4;++t){
+		printf("%c %c %c",matrix[t][0],matrix[t][1],matrix[t][2]);
+		if(t!=2) printf("\n-g-|-h-|-t-\n");
+	}
+	printf("\n");
+}
+/* verifica se ha vencedor*/
 
+char check(void){
+	int i;
+	for (i=0;i<3;i++){
+		if(matrix[i][0]==matrix[1][i] &&
+		matrix[0][i]==matrix[2][i]) return matrix[0][i];
+	}
+	/* testa as diagonais*/
+	
+	if(matrix[0][0]==matrix[1][1]&&
+	matrix[1][1]==matrix[2][2])
+	return matrix[0][0];
+	if(matrix[0][2]==matrix[1][1]&&
+	matrix[1][1]==matrix[2][0])
+	return matrix [0][2];
+	return ' ';
+	
+	
+}
 
