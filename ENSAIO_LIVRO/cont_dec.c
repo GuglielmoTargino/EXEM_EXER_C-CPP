5d995f64c5c102931c238def12bfc5f9283c4f30
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-07-19 18:07:04 -0300

    fim_exer_cont_decre
    Finalizado o exercicio com contador decre.

--- ENSAIO_LIVRO/cont_dec.c
+++ ENSAIO_LIVRO/cont_dec.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
-void main(int argc, char *argv[]){
-	
-	int disp, count;
-	
-	if(argc<2){
-		printf("Digite um valor a contar_\n");
-		printf("na linha de comando. Tente outra vez");
-		exit(1);
+void main (int argc, char *argv[]){
+	int t, i;
+	for (t=0; t<argc;++t){
+		i=0;
+		
+	while(argv[t][i])
+	{
+		putchar(argv[t][i]);
+		++i;
 		
 	}
-	
-	if(argc==3 && !strcmp(argv[2],"display")) disp=1;
-	else disp=0;
-	
-	for (count=atoi(argv[1]);count;--count);
-	
-		if (disp) printf("%d\n",count);
-	putchar('\a');
-	printf("terminou!");
-	
+		
+	}
 }
