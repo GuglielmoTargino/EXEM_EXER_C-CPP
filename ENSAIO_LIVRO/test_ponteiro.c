6d338031cea1318dcffe73fd3d961b727e5cae0d
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-07-20 20:09:58 -0300

    fim_teste_ponteiro
    Finalizado o exercicio com um exemplo de ponteiro.

--- ENSAIO_LIVRO/test_ponteiro.c
+++ ENSAIO_LIVRO/test_ponteiro.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
+char *match(char c, char *s); /* modelo de prototipo*/
+
+
 void main (void){
+
 	char s[80], *p, ch;
 	gets(s);
 	ch=getchar();
-	if(*p) printf("%s", p);
-	else printf("Não encontrado");
+	p=match(ch, s);
+	if(*p){
 	
+ 	 printf("tem %c em %s",ch, s);
+ 	  	 
+ 	}else
+ 	printf("nao encontrado %c em %s", ch, s);	
+}
+
+char *match(char c, char *s){
+	while(c!=*s && *s) s++;
+	return (s);
 }
