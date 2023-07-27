09181a5f0b0cf3b534295d631f3375e8a8aefdb7
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-07-27 18:32:51 -0300

    fim_relogio_struct
    Finalizado o programa ce relogio com struct

--- ENSAIO_LIVRO/relogio_struct.c
+++ ENSAIO_LIVRO/relogio_struct.c
@@ -29,4 +29,23 @@ void update(struct my_time *t){
 		t->seconds=0;
 		t->minutes++;
 	}
+	if(t->minutes==60){
+		t->minutes=0;
+		t->hours++;
+	}
+	if(t->hours==24) t->hours=0;
+	delay();
 }
+
+void display(struct my_time *t){
+	printf("%02d:",t->hours);
+	printf("%02d:",t->minutes);
+	printf("%02d\n", t->seconds);
+}
+void delay(void){
+	long int t;
+	// mude isso como preferir
+	for(t=1;t<DELAY;++t);
+}
+
+
