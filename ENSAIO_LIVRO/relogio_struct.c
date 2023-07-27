4258c0769af748275725fa71d73b80d1afee6b96
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-07-27 17:39:12 -0300

    ok_met_second
    Finalizado o bloco do metodos para contagem de segundos.

--- ENSAIO_LIVRO/relogio_struct.c
+++ ENSAIO_LIVRO/relogio_struct.c
@@ -6,3 +6,27 @@ struct my_time{
 	int minutes;
 	int seconds;
 };
+void display(struct my_time *t);
+void update(struct my_time *t);
+void delay(void);
+
+void main (void){
+	struct my_time systime;//systime é uma variável criada e declarada da estrutura my_time
+                           //e por isso recebe seus elementos.
+	
+	systime.hours=0;
+	systime.minutes;
+	systime.seconds;
+	for(; ; ){
+		update(&systime);
+		display(&systime);
+	}
+	
+}
+void update(struct my_time *t){
+	t->seconds++;
+	if(t->seconds==60){
+		t->seconds=0;
+		t->minutes++;
+	}
+}
