a2c65b3dfa16489c677b3438bc4d0b0aecac1dee
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-07-28 17:21:55 -0300

    test_struct
    Exercício com estrutura de variáveis. apenas

---
+++ ENSAIO_LIVRO/test_struct.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct gato{
+	char dente;
+	char unha;
+	char pelo;
+};
+
+void main(void){
+	struct gato pet;
+	printf("item %s",pet.dente);
+	printf("item %s",pet.pelo)
+	printf("item %s",pet.unha);
+}
