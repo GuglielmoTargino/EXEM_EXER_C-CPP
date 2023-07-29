8c93959669ad3f8e99307311885fe3c5d17e22af
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-07-28 21:59:35 -0300

    fim_teste_struct
    Finalizado o  teste com variavel struct.

--- ENSAIO_LIVRO/test_struct.c
+++ ENSAIO_LIVRO/test_struct.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 struct gato{
-	char dente;
+	char dente[80];
 	char unha;
 	char pelo;
-};
+}pet;
 
 void main(void){
-	struct gato pet;
-	printf("item %s",pet.dente);
-	printf("item %s",pet.pelo)
-	printf("item %s",pet.unha);
+	
+	//struct gato pet;
+	
+	pet.dente[0]='f';
+	pet.dente[1]='o';
+	pet.dente[2]='r';
+	pet.dente[3]='t';
+	pet.dente[4]='e';
+	pet.dente[5]='\0';
+	pet.pelo='p';
+	pet.unha='u';
+	printf("item %s\n",pet.dente);
+	printf("item %c\n",pet.pelo);
+	printf("item %c",pet.unha);
 }
