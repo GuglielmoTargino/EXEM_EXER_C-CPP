8e6158681007eab5f66137f9fc62e402763f2362
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-08-03 20:49:22 -0300

    fim_exer_getchar
    Finalizado o exercício com função getchar.

--- ENSAIO_LIVRO/teste_getchar_putchar.c
+++ ENSAIO_LIVRO/teste_getchar_putchar.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
 
 void main(void){
-	char ch;
-	printf("digite um caracter ou um ponto para finalizar\n");
-	do {
-		ch=getchar(); //pega o caracter digitado no teclado e salva em ch
-		
-			if(isupper(ch)) ch=tolower(ch);
-			else ch=toupper(ch);
-			
-			putchar(ch);
-		
-	}while (ch!='.');
+	char str[80];
+	char g[80];
+	printf("digite uma frase");
+	gets(str);
+	printf("o comprimento de str é %d", strlen(str));
 }
