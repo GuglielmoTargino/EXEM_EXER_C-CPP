bc2c6661f23af3cb08d566763fc9eddcb3046759
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-08-03 19:18:33 -0300

    teste_sizeof
    Exercício de teste com sizeof

---
+++ ENSAIO_LIVRO/sizeof_teste.c
@@ -0,0 +1,14 @@
+#include <stdio.h1>
+	struct u{
+		char i;
+		int g;
+		float f;
+	};
+void main(void){
+
+	struct u ght;
+	printf("%c",sizeof(ght.i));
+	printf("%d",sizeof(ght.g));
+	printf("%f",sizeof(ght.f));
+	
+}
