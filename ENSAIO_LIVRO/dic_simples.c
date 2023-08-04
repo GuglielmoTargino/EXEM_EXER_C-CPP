89cc6353bccd919bf3b271132fa5e28f6aaea7e6
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-08-04 17:58:59 -0300

    fim_dic_simples
    Finalizado o exercício com dicionario simples

--- ENSAIO_LIVRO/dic_simples.c
+++ ENSAIO_LIVRO/dic_simples.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<conio.h>
-#include<string>
+#include<string.h>
 
 //lista simples de palavras
 char *dic[][40]={
@@ -10,3 +10,26 @@ char *dic[][40]={
 "um dispositivo de comunicação","aviao","uma maquina voadora",
 "",""/*nulo termona a lista*/};
 
+void main(void){
+	char word[80],ch;
+	char **p;
+	do{
+		puts("\ndigite uma palavra_");
+		gets(word);
+		p=(char**)dic;
+		//acha a palavra no dicionario
+	   do{
+			if(!strcmp(*p,word)){
+			puts("significado:");
+			puts(*(p+1));
+			break;
+		}if(!strcmp(*p,word))
+		break;
+		p=p+2; //\v\nc\ \ lista
+		
+		}while (*p);
+		if(!*p)puts ("a palavra não está no dicionario");
+		printf("outra palavra? S/N");
+		ch=getche();
+	}while(toupper((ch)!='N'));
+}
