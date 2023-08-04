76f9c5edf0afd603cff8171d082f8c91683adc1e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-08-04 17:32:43 -0300

    dic_simples
    Exercício simples de um dicionário.

---
+++ ENSAIO_LIVRO/dic_simples.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<conio.h>
+#include<string>
+
+//lista simples de palavras
+char *dic[][40]={
+"atlas","um livro de mapas","carro",
+"veiculo motorizado","telefone",
+"um dispositivo de comunicação","aviao","uma maquina voadora",
+"",""/*nulo termona a lista*/};
+
