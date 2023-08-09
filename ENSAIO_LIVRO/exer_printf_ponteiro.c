bf92c89984700fba25f8ec20eee161980c463dba
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-08-09 20:38:21 -0300

    fim_printf_ponteiro
    Finalizado o exercício printf com ponteiro.

--- ENSAIO_LIVRO/exer_printf_ponteiro.c
+++ ENSAIO_LIVRO/exer_printf_ponteiro.c
@@ -3,6 +3,6 @@
 void main(void){
 	char *p;
 	printf("digite um endereço_");
-	scanf("%p",&p);
-	printf("na posição %p tem %c\n",p,*p);
+	scanf("%p",&p);//deve-se digitar um endereço qualquer em hexadecimal
+	printf("na posição %p tem %c\n",p,*p);// mostra ao que tem no endereço digitado.
 }
