f25851f1dcad25432f21af65119a7f0b6367c296
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-08-09 21:05:50 -0300

    fim scanset
    Fim exercício scanset

--- ENSAIO_LIVRO/exer_scanset.c
+++ ENSAIO_LIVRO/exer_scanset.c
@@ -3,6 +3,7 @@
 void main (void){
 	int i;
 	char str[80],str2[80];
-	scanf(%d%[abcdefg]%s,&i,str,str2);
-	scanf("%d %s %s"i,str,str2);
+	scanf("%d%[guga]%s",&i,str,str2);// %d se refere &i, %[guga] se refere str, %s se refere str2.
+	printf("%d %s %s",i,str,str2);
+	
 }
