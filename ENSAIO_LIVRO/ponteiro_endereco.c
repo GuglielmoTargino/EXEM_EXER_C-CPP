9147b1462d38d7ff3036196438ea71fd6f75066a
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-08-09 21:50:58 -0300

    exer_ponteiro
    Exercício com ponteiro

--- ENSAIO_LIVRO/ponteiro_endereco.c
+++ ENSAIO_LIVRO/ponteiro_endereco.c
@@ -3,13 +3,21 @@
 void main(void){
 	
 	int target, source;
-	int *m;/* forma de declarar uma variável como ponteiro*/
+	int *m;/* forma de declarar uma ptr( variável ponteiro)*/
+	float a,b,r;
+	float *ptr_a, *ptr_b;
+	
+	a=7.3;
+	b=11.5;
+	ptr_a=&a;
+	ptr_b=&b;
+	r=*ptr_a+*ptr_b;
 	
 	target=8; 
 	m=&target; /* m recebe o endereço de target*/
 	source=*m; /* source recebe o valor registrado no enderço passado em m*/
 	
 	printf("o endereço de target é %d\n",m);
-	printf("o valor de target é %d", target);	
-	
+	printf("o valor de target é %d\n", target);	
+	printf("soma de a com b utilizando ponteiro é %.2f",r);
 }
