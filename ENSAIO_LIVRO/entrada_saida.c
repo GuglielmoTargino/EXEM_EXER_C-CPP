9ffb26abf75749ce5090442dc0f021affb6135bb
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-08-15 17:17:20 -0300

    exer_entrada_saida
    Exercício com entrada e saida de arquivos

---
+++ ENSAIO_LIVRO/entrada_saida.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char *argv[]){
+	
+	FILE *fp;
+	char ch;
+	if(argc!=2){
+		printf("vc esqueceu de digitar um arquivo_");
+		exit(1);
+	}
+	
+}
