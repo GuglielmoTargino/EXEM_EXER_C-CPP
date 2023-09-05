219552157d196cedf0c4373fff846af19fdd1f16
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-09-05 19:18:22 -0300

    exerc_fputs
    exercicio com função puts

---
+++ ENSAIO_LIVRO/exerc_fputs.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void main(void){
+	char str[80];
+	FILE *fp;
+	
+	if((fp=fopen("teste","w"))==NULL){
+		printf("o arquivo não pode ser aberto.\n");
+		exit(1);
+	}
+	do{printf("digite uma string (CR para sair):\n");
+	fputs(str,fp);
+	
+	}while(*str!='\n');
+}
