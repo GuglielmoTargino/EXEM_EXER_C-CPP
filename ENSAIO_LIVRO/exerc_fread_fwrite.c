68216604faa30173ea9794cc6593a51bf0316129
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-09-19 18:18:37 -0300

    exerc_fread_fwrite
    iniciado o exercício com função read e write.

---
+++ ENSAIO_LIVRO/exerc_fread_fwrite.c
@@ -0,0 +1,22 @@
+/*
+escreve alguns dados em um arquivo de disco
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void){
+	
+	FILE *fp;
+	double d=1.23;
+	int i=101;
+	long l=123023L;
+	if((fp2=fopen("test","wb+"))==NULL){
+		printf(" o arquivo não pode ser aberto.\n");
+		exit(1);
+	}
+	fwrite
+	
+	
+	
+}
