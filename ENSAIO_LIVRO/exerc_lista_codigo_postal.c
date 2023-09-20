3d5b3ade7a6a05f25ad91bbc148282494ec83357
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-09-20 16:54:01 -0300

    fim_edição_lista_postal
    Finalizada a finalização da edição do exercício da lista de endereço postal

--- ENSAIO_LIVRO/exerc_lista_codigo_postal.c
+++ ENSAIO_LIVRO/exerc_lista_codigo_postal.c
@@ -8,15 +8,131 @@ Versão:v0
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdtype.h>
+#include <ctype.h>
 #include <string.h>
 
 #define SIZE 100
 
 struct list_type{
+	char name[40];
+	char street[40];
+	char city[30];
+	char state[3];
+	char zip[10];	
 	
+}list[SIZE];
+
+int menu(void);
+void init_list(void),enter(void);
+void display(void),save(void);
+void load(void);
+
+void main(void){
+	char choice;
+	init_list();
 	
-};
+	for(;;){
+		choice=menu();
+		switch(choice){
+			case'e':  enter();
+			break;
+			case 'd': display();
+			break;
+			case 's': save();
+			break;
+			case 'l': load();
+			break;
+			case 'q': exit(0);
+			
+		}
+	}
+}
+
+// inicia a lista 
+
+void init_list(void){
+	int t;
+	for (t=0; t<SIZE;t++) *list[t].name='\0';
+	//um nome de comprimento zero significa vazio
+	
+}
+//insere nomes na lista
+void enter(void){
+	int i;
+	for(i=0;i<SIZE;i++)
+       if(!*list[i].name)break;
+    if(i==SIZE){
+    	printf("lista cheia\n");
+    	return;    	
+	}
+	printf("nome:");
+	gets(list[i].name);
+	
+	printf("rua:");
+	gets (list[i].street);
+	
+	printf("cidade");
+	gets (list[i].city);
+	
+	printf("estado");
+	gets(list[i].state);
+	
+	printf("cep");
+	gets(list[i].zip);
+	
+}
+
+//mostra a lista
+
+void display(void){
+	
+	int t;
+	for(t=0;t<SIZE;t++){
+		if(*list[t].name){
+			printf("%s\n",list[t].name);
+			printf("%s\n",list[t].street);
+			printf("%s\n",list[t].city);
+			printf("%s\n",list[t].state);
+			printf("%s\n\n",list[t].zip);
+			
+		}
+	}
+
+}
+//salva a lista
+
+void save(void){
+	FILE *fp;
+	int i;
+	if((fp=fopen("maillist","wb"))==NULL){
+		printf("o arquivo não pode ser aberto\n");
+		
+	}
+	for (i=0;i<SIZE;i++);
+	 	if(*list[i].name)
+	 	
+		  if(fwrite(&list[i],sizeof(struct list_type),1,fp)!=1)
+		  printf("erro de escrita\n");
+    fclose(fp);
+    
+}
+
+// pega uma seleção
+menu(void){
+	char s[80];
+	
+	do{
+		printf("(i) inserir");
+		printf("(v) visualizar");
+		printf("(c) carregar");
+		printf("(s) salvar");
+		printf("(t) terminar");
+		printf("escolhar");
+		gets(s);		
+		
+	}while (!strchr("ivcst", tolower(*s)));
+	return tolower(*s);
+}
 
 
 
