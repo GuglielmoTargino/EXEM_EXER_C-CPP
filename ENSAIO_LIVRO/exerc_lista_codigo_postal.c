7c237b7b3fe2b25d1ffc8086dc90c5d8217204b4
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-09-20 15:44:54 -0300

    exer_lista_codigo_postal
    Exercício com uma lista de codigo postal

---
+++ ENSAIO_LIVRO/exerc_lista_codigo_postal.c
@@ -0,0 +1,23 @@
+/*
+Exercicio do livro linguagem c.
+Programa transcrito do original para listar codigos postais.
+Digitador: Guglielmo Henriques Targino.
+Data:20/09/23.
+Versão:v0
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdtype.h>
+#include <string.h>
+
+#define SIZE 100
+
+struct list_type{
+	
+	
+};
+
+
+
+
