0c48f25553fb196bbfa54eea741a8dfa0cd510ca
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-09-29 17:37:34 -0300

    exer_fscanf_fprintf
    Iniciado o exercício com as funções fscanf e fprintf.

---
+++ ENSAIO_LIVRO/exem_fscanf_fprintf.c
@@ -0,0 +1,36 @@
+/*	exemplo test de fcanf() e fprointf().
+	Estudante: Guglielmo Henriques Targino
+	Data: 29/09/23
+	Versão: V0.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+
+void main(void){
+	
+	FILE *fp;
+	char s[80];
+	int t;
+	if((fp=fopen("test", "w"))==NULL){
+		
+		printf("o arquivo não abre\n");
+		exit(1);
+		
+	}
+	printf("digite uma string e um numero");
+	fscanf(stdin,"%s%d,s,&t");
+	
+	fprintf(fp, "%s%d",s,t);
+	fclose(fp);
+	
+	if((fp=fopen("test","r"))==NULL){
+		printf("o arquivo não abre");
+		exit(1);
+	}
+	fscanf(fp,"%s%d",s,&t);
+	fprintf(stdout,"%s%d",s,t);
+	
+	
+}
