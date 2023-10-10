3b48741152e2406edef5dd34baa4b2f6dc2438c1
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-10-10 19:19:46 -0300

    ensaio_c++
    ensaio com linguagem de programação c++

---
+++ ENSAIO_LIVRO/teste1c++.cpp
@@ -0,0 +1,17 @@
+/* meu primeiro programa em c++
+autor: Guglielmo Henriques Targino
+data:10/10/23
+versão: V0
+
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main(){
+	
+	cout<<"olá mundo\n";
+	system("pause");
+	return 0;
+}
