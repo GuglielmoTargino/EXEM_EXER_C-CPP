aac6f362d44482744080e515cf9eb27f3b2f48e2
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-10-20 19:20:39 -0300

    teste_EOF
    Exercício com uso da função EOF.

---
+++ ENSAIO_LIVRO/teste_EOF.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+void main (int argc, char *argv){
+	
+	FILE *fp;
+	char ch;
+	
+	if((fp=fopen(argv[1],"r"))==NULL){
+		printf("arquivo não abre\n");
+		exit(1);
+	}
+	while((ch=fgetc(fp))!=EOF){
+		printf("%c",ch);
+	}
+	fclose(fp);
+}
