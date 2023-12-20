84c57fc0652aa130ecc3ece32d4940fd267d42a2
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-20 19:37:45 -0300

    fim_exer_fprintf
    Finalizado o exercicio com a função fprintf;

--- ENSAIO_LIVRO/exer_fprintf.c
+++ ENSAIO_LIVRO/exer_fprintf.c
@@ -3,15 +3,15 @@
 //Data: 20/dez/23
 //Versão: v0
 
-#include <stdi.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 void main (void){
-	
+	FILE *fp;
 	if((fp=fopen("teste","wb"))==NULL){
 		printf("arquivo nao abre");
-		exit(1)
+		exit(1);
 	}
-	fprintf(fp,"isto é um teste %d %f",10,20.01);
+	printf("isto é um teste %d %f",10,20.01);
 	fclose(fp);
 }
