7b76263079d207490ab9ce0f5a7a704402622b1c
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-21 19:59:23 -0300

    err_caractere_acentuado
    Resolvendo Erro com os caracteres acentuados.

--- ENSAIO_LIVRO/exer_soma_media_tres_num.cpp
+++ ENSAIO_LIVRO/exer_soma_media_tres_num.cpp
@@ -6,4 +6,35 @@ versão: v0
 */
 
 
+#include <iostream>
+#include<iomanip> //definição necessária para usar as funções setw e setf.
+#include <locale> //definição para caracteres acentuados
+
+using namespace std;
+int main(){
+	std::locale::global(std::locale("")); //configura locale de acentuação
+	std::wcout.imbue(std::locale());		//configura a saida do locale
+	
+	
+	float soma,prod,media,n1,n2,n3;
+	
+	cout<<"digite três numeros_";
+	cin>>n1>>n2>>n3;
+	soma=n1+n2+n3;
+	prod=n1*n2*n3;
+	media=soma/3;
+	
+	cout<<"soma é_"<<soma<<endl;
+	cout.setf(ios::fixed | ios::showpoint); // mostra reais em ponto flxo
+	cout.precision(2);						//formata para 2 casas decimais
+	
+//setw(8) fixa tamanho da representação até 8 digitos
+	
+	std::wcout <<"multiplicação é_"<<(unsigned)prod<<endl;
+	cout<<"media é_"<<media<<setw(8)<<endl;
+	
+	
+}
+
+
 
