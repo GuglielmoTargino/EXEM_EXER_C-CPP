14e72bfc734675f992178a67c3c19bb6b3bb5ce5
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-21 18:03:46 -0300

    fim_exer_funcao_freopen
    Finalizado o exercicio com a função freopen

--- ENSAIO_LIVRO/exer_funcao_freopen.c
+++ ENSAIO_LIVRO/exer_funcao_freopen.c
@@ -8,4 +8,12 @@ versão:v0
 
 void main(void){
 	
+	FILE *fp;
+	printf("isso é mostrado na tela");
+	if((fp=freopen("out","w",stdout))==NULL){
+		printf("o arquivo não abre");
+		exit(1);
+	}
+	printf("isso será escrito no arquivo out");
+	fclose(fp);
 }
