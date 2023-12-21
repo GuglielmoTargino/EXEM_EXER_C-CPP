d830b1577d360eb2eade11c97b41479e32db3f5d
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-21 17:24:12 -0300

    exer_funcao_fread
    Iniciado exercicio com função fread

---
+++ ENSAIO_LIVRO/exer_funcao_fread.c
@@ -0,0 +1,19 @@
+//exercicio com função fread()
+//Aluno:guglielmo Targino
+// Data: 21dez23
+//versão: v0
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (void){
+	FILE *fp;
+	float bal[10];
+	if((fp=fopen("teste","rb"))==NULL){
+		printf("O arquivo não abre");
+		exit(1);
+	}
+	if
+	
+	
+}
