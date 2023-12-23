6886792e666d987a195e70bedf187ac703da77d7
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-22 22:46:58 -0300

    fim_exem_div_numeros
    Finalizado o exem com divisão de numeros.

---
+++ ENSAIO_LIVRO/exem_numerador.cpp
@@ -0,0 +1,31 @@
+/*
+Exemplo da apostila cpp
+Aluno: Guglielmo Targino
+Data: 22dez23
+Versão:v0
+
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <locale>
+
+using namespace std;
+
+int main(){
+	cout.setf(ios::fixed|ios::showpoint);
+	cout.precision(3);
+	setlocale(LC_ALL,"Portuguese");
+	float nme,den;
+	float resu;
+	
+	cout<<"digite o numerador e depois o denominador"<<endl;
+	cin>>nme>>den;
+	if(den==0)
+       cout<<"denominador não pode ser zero. Tente outra vez";
+       
+    else {
+    	resu=nme/den;
+    	cout<<"o resultado da fração é_"<<resu;
+	}
+}
