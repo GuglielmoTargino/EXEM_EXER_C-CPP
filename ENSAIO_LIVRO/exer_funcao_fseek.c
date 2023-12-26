0879f48c0ac0ca4f6dae636533f283ebea07ba45
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-26 16:49:44 -0300

    ini_exer_funcao_getcha
    Inicício de exercício com a função getchar

---
+++ ENSAIO_LIVRO/exer_funcao_fseek.c
@@ -0,0 +1,20 @@
+/*
+Exercíio com a função fseek()
+Aluno: Guglielmo Targino
+Data: 26dez23
+Versão:v0
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct addr{
+	char name[40];
+	char street[40];
+	char city[40];
+	char state[40];
+	char zip[10];
+	
+}info;
+
+
