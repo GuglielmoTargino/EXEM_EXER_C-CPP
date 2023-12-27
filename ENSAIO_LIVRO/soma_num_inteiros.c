b05e8037b11899be97d17605f711193d9060930b
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-27 18:17:38 -0300

    exem_soma_num_intei
    Exem de exercício que soma numeros inteiros de um arquivo.

---
+++ ENSAIO_LIVRO/soma_num_inteiros.c
@@ -0,0 +1,14 @@
+/*
+exercício do livro c completo.
+ele lê numeros inteiros do arquivo teste
+e apresenta a soma deles.
+Aluno: Guglielmo Targino.
+Data: 27dez23
+Versão:v0
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
