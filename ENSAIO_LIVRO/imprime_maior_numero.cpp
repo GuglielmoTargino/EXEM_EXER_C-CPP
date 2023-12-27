b250f60b9f43fbbefd421bb22c0efc7e68080970
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-27 20:24:10 -0300

    ok_imprimir_maior_numero
    Finalizado o programa para se imprimir o maior número.

--- ENSAIO_LIVRO/imprime_maior_numero.cpp
+++ ENSAIO_LIVRO/imprime_maior_numero.cpp
@@ -5,4 +5,34 @@ Data: 27dez23.
 Versão:v0
 */
 
+#include <iostream>
+#include <locale>
 
+using namespace std;
+
+int main(void){
+	setlocale(LC_ALL,"Portuguese");
+	
+	int num1,num2,num3,maior;
+	cout<<"digite três números_"<<endl;
+	
+	cin>>num1>>num2>>num3;
+	
+	if(num1>num2){
+		if(num1>num3)
+		maior=num1;
+	
+	}else if(num2>num1){
+		 if(num2>num3)
+		  maior=num2;
+	}else if(num3>num1){
+		  if(num3>num2)
+	  	maior=num3;
+		   
+	}
+		 
+	
+	cout<<"maior número_"<<maior;
+
+
+}
