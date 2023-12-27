8008627140fe7af513b92029f488cdf127ab4f3e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-12-27 18:57:35 -0300

    ok_exem_soma_numeros
    Correção de modo de escrita em fopen().

--- ENSAIO_LIVRO/soma_num_inteiros.c
+++ ENSAIO_LIVRO/soma_num_inteiros.c
@@ -10,5 +10,29 @@ Versão:v0
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+void main(void){
+	setlocale(LC_ALL,"Portuguese");
+	FILE *fp;
+	int sum=0;
+	fp=fopen("ght","w"); /* modo de leitura deve ser "w"
+						 	pois o arquivo não existe ainda.
+						 	para ser lido.*/
+	if(fp==NULL){
+			printf(" arquivo não abre_");
+			exit(1);
+	
+	}
+	while(!feof(fp)){
+		sum=getw(fp)+sum;
+		printf("a soma é %d", sum);
+		
+	}
+	
+	fclose(fp);
+
+	
+}
 
 
