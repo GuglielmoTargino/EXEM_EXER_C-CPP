911dfa755b82c90da657f5ebf6722a52da2279f2
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-02 21:29:52 -0300

    inicio_test_printf
    Iniciado o teste do exercício com printf.

---
+++ ENSAIO_LIVRO/teste_printf.c
@@ -0,0 +1,24 @@
+/*
+Exercício do livro c completo e total.
+Aluno: Guglielmo Targino.
+Data: 02jan24
+Versão: v0
+*/
+#include <stdio.h>
+#include <locale.h>
+
+void main(void){
+	
+	setlocale(LC_ALL,"Portuguese");
+	
+	char a='g';
+	char b'ght';
+	int c=4;
+	
+	printf(" a palavra é_")
+	
+	
+	
+	
+	
+}
