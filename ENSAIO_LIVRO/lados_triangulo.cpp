a4d4b560af78cc0760727b12d9dba83f80fd6630
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-03 23:44:37 -0300

    fim_exem_lados_triangulo
    Finalizao o execício para medir os lados de um triangulo.

--- ENSAIO_LIVRO/lados_triangulo.cpp
+++ ENSAIO_LIVRO/lados_triangulo.cpp
@@ -13,6 +13,26 @@ Versão: v0
 
 using namespace std;
 int main (void){
+	setlocale(LC_ALL,"Portuguese");
 	
+	int lado1,lado2,lado3;
+	int s1,s2,s3;
+	cout<<"digite três numeros_";
+	cin>>lado1>>lado2>>lado3;
+	 //calcula os quadrados dos lados.
+	 
+	 s1=lado1*lado1;
+	 s2=lado2*lado2;
+	 s3=lado3*lado3;
+	 
+	 //testa se a condição é um triangulo reto.
+	 
+	 if(lado1>0 && lado2>0 && lado3>0){
+	 	if(s1==s2+s3 || s2==s1+s2 || s2==s1+s3){
+	 		cout<< "triangulo reto\n";
+		 }else{
+		 	cout<<"Lados não formam triangulo reto\n";
+		 }
+	 }
 	
 }
