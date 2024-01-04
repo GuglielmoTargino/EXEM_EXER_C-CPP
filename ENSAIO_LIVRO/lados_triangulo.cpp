52d64d6c1d51e58f47332800ce958203c21ff2bb
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-03 23:21:22 -0300

    ini_exem_lados_trangulo
    Iniciado o exemplo de exercício para determinar os lados do triangulo,

---
+++ ENSAIO_LIVRO/lados_triangulo.cpp
@@ -0,0 +1,18 @@
+/*
+Exemplo do livro CPP.
+Aluno: Guglielmo Targimo.
+Data: 03jan24.
+Versão: v0
+
+
+*/
+
+
+#include <iostream>
+#include <locale>
+
+using namespace std;
+int main (void){
+	
+	
+}
