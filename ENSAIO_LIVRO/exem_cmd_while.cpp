61b3234fbfbd444f54d3fb49901b1c22bd22c491
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-04 22:02:57 -0300

    exem_cmd_while
    Exemplo de exercício com comando while.

---
+++ ENSAIO_LIVRO/exem_cmd_while.cpp
@@ -0,0 +1,16 @@
+/*
+Exercício comando while da apostila.
+Aluno: Guglielmo Targino.
+Data: 04jsn24
+Versão: v0
+
+*/
+
+#include <iostream>
+#include <locale>
+
+using namespace std;
+int main(void){
+	
+	
+}
