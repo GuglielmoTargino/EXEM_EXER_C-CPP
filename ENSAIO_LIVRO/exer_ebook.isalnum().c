225ababf3e20a464340d65716618520a0be26c5e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-15 23:21:18 -0300

    fim_exerc_isalmun
    Finalizado o exercício com a função isalnum().

--- ENSAIO_LIVRO/exer_ebook.isalnum().c
+++ ENSAIO_LIVRO/exer_ebook.isalnum().c
@@ -5,5 +5,21 @@ Eercício do ebook com função isalnum().
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main(void){
+	setlocale(LC_ALL,"Portuguese");
+	
+	char ch;
+	
+	for(;;){
+		
+		ch=getc(stdin);
+		if(ch==' ') break;
+		if (isalnum(ch)) printf("%c é alfanumerico\n",ch);
+		printf("Precione um espaço para sair.");
+	}
+}
 
 
