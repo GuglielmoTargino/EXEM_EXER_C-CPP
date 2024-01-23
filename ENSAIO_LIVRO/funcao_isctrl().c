2847567821fa54c16eb3ab0c38d432b1f7f2de2c
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-22 22:14:31 -0300

    Create funcao_isctrl().c
    Exercício com a função isctrl().

---
+++ ENSAIO_LIVRO/funcao_isctrl().c
@@ -0,0 +1,24 @@
+/*
+
+exercicio do livro.
+
+função isctrl().
+
+Aluno: Guglielmo Targino.
+Data: 22jan24.
+Versão: v0.
+
+
+
+*/
+
+
+Void main(void){
+	
+	char ch;
+	ch=getchar();
+	
+	if(;;){
+		if(ch==' ') break;
+	} printf(" %c é um caracter de controle",ch);
+}
