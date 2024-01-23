f97b690dbda5283671b9107d01c4fc3a0d984eb1
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-22 22:45:08 -0300

    Create exer_funcoes.cpp
    Iniciado o exercício com funções.

---
+++ ENSAIO_LIVRO/exer_funcoes.cpp
@@ -0,0 +1,29 @@
+/*
+exercício do livro c++.
+Aluno: Guglielmo Targino.
+Data: 22jan24
+Versão:v0.
+
+
+
+*/
+
+#include <iostrem>
+#include <locale>
+
+
+using namespace std;
+
+void cumprimentar(char inic, char inic2){
+	cout<<"Olá"<<inic2<<"!"<<endl;
+	
+}
+
+void main(void){
+	
+	
+	setlocale(LC_ALL,"Portuguese");
+	
+	
+	
+}
