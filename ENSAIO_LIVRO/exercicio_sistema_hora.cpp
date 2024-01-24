cccb8bedf258ead4d103102ac02dd20a6116d800
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-23 22:59:19 -0300

    Treino com funcao de soma

---
+++ ENSAIO_LIVRO/exercicio_sistema_hora.cpp
@@ -0,0 +1,36 @@
+/*
+
+continua o exercicio com acesso qao sistema de hora local
+
+Aluno:Guglielmo Targino.
+Data: 23/jan/24.
+Versão: v0.
+
+
+*/
+
+#include <iostream>
+#include <locale>
+
+using namespace sts;
+
+int pegavalor(void){
+	int valor;
+	cout<<"digite um valor";
+	cin>>valor;
+	return valor;
+}
+
+int main (void){
+	
+	int a,b;
+	
+	a=pegavalor();
+	b=pegavalor();
+	
+	cout<<"soma="<<a+b<<endl;
+	
+	
+	
+	
+}
