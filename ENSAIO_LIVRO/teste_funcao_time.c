db6a97c88b03a7d93b86d83de402606b8e651db0
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-25 23:03:01 -0300

    Resolvendo codigo de Falha.

--- ENSAIO_LIVRO/teste_funcao_time.c
+++ ENSAIO_LIVRO/teste_funcao_time.c
@@ -9,8 +9,20 @@ Versão: v0.
 
 #include <time.h>
 #include <stdio.h>
+#include <locale.h>
+
 
 void main (void){
+	
+	setlocal(LC_ALL,"Portuguese");
+	
+	time_t start,end;
+	long unsigned t;
+	start=time(NULL);
+	
+	for (t=0;t<10;t++);
+	end =time(NULL);
+	printf(" O laço usou %f segundos.\n", difftime(end,start));
 	
 	
 }
