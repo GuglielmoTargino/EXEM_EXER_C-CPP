ab456213f0df7f3a5a61f109a200f6f0f641041b
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-25 22:48:06 -0300

    Teste em c com função de acesso ao local time

---
+++ ENSAIO_LIVRO/teste_funcao_time.c
@@ -0,0 +1,16 @@
+/*
+Exexrcício com função que acesso o local time
+
+Aluno: Guglielmo Targino.
+Data:25jan24.
+Versão: v0.
+
+*/
+
+#include <time.h>
+#include <stdio.h>
+
+void main (void){
+	
+	
+}
