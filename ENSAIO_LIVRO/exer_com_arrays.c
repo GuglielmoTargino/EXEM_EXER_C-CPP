6a05540fd39eb4e4ea6a41c40a28227f62468bc1
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-01-26 23:11:34 -0300

    testes com função getch

---
+++ ENSAIO_LIVRO/exer_com_arrays.c
@@ -0,0 +1,31 @@
+/*
+Exercício com arrays
+
+Autor: Guglielmo Targino:
+Data: 26jan24
+Versão: v0.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (void){
+	
+	char aluno[4][3];
+	int i,j,c=0;
+	
+	
+	printf("digite tres notas_");
+	
+	for(i=0;i<4;i++){
+		c=getch();
+		printf("o numero é %d ",c);
+		
+	}
+	
+}
+
+
+
+
