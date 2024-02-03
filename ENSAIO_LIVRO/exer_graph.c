24393f6f3f142e1c9b6bdf8973a26f33ccb710d2
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-02-02 22:11:35 -0300

    Teste com funcao grafico.

---
+++ ENSAIO_LIVRO/exer_graph.c
@@ -0,0 +1,20 @@
+/*
+
+Exercicio com funcao arc();
+
+Aluno: Guglielmo Targino.
+
+Data: 02fev24.
+versão: v0.
+*/
+
+#include <graph.h>
+#include <conio.h>
+
+ void main (void){
+ 	
+ 	_setvideomode(_VRES16COLOR);
+ 	_arc(100,100,200,200,100,100,200,200);
+ 	getche();
+ 	_setvideomode(_DEFAULTMODE);
+ }
