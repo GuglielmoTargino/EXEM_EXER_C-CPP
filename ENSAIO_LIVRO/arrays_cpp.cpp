0fb5023caef544a0766e8c59b01a84b9105fcf40
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-02-14 12:20:55 -0300

    finalizado o exercício com arrays em cpp

--- ENSAIO_LIVRO/arrays_cpp.cpp
+++ ENSAIO_LIVRO/arrays_cpp.cpp
@@ -8,6 +8,7 @@ Versão: v0.
 
 #include <iostream>
 #include <iomanip>
+#include <locale>
 
 #define altura 10
 #define largura 10
@@ -15,7 +16,7 @@ Versão: v0.
 using namespace std;
 
 int main (void){
-	
+	setlocale(LC_ALL,"Portuguese");
 	int x,y;
 	int matriz[altura][largura];
 	
@@ -24,30 +25,36 @@ int main (void){
 			matriz[y][x]=y*x;
 		
 		}
-	cout<<"\n a tabela é _\n";
+	
 	}
 	// imprime a matriz com zero e a coordenada escolhida
 	
-
+	cout<<"\n a tabela é _\n";
 	
 	cout<<setw(6)<<0;
 	for (x=1;x<largura;x+=1){
 		cout<<setw(3)<<x;
-	cout<<endl;
-	cout<<" ";
+	//cout<<endl;
+	cout<<"";
 		
 	}
+	cout<<endl;
 	
 	for (x=0; x<3*largura;x+=1){
 		cout<<"_";
-		cout<<endl;
+		
 	}
 	
+	cout<<endl;
+	
 	for(y=0;y<altura;y+=1){
+		cout<<endl;
 		cout <<setw(2)<<y<<"|";
+		
 		for(x=0;x<largura;x+=1){
+			
 			cout<<setw(3)<<matriz[y][x];
-			cout<<endl;
+			//cout<<endl;
 		}
 	}
 	
