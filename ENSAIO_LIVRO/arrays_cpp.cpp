499eccd255864613290587a7b316fd1d6b7741ab
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-02-13 22:39:25 -0300

    continua o exercício com arrys com cpp

--- ENSAIO_LIVRO/arrays_cpp.cpp
+++ ENSAIO_LIVRO/arrays_cpp.cpp
@@ -6,41 +6,50 @@ Versão: v0.
 
 */
 
-#define <iostream>
+#include <iostream>
+#include <iomanip>
 
-#define altura 5
-#define largura 5
+#define altura 10
+#define largura 10
 
 using namespace std;
 
 int main (void){
 	
 	int x,y;
-	char matriz[altura][largur];
+	int matriz[altura][largura];
 	
 	for(y=0;y<altura;y+=1){
-		for(x=0x<largura;x+=1){
-			matriz[y][x]=0;
+		for(x=0;x<largura;x+=1){
+			matriz[y][x]=y*x;
+		
 		}
+	cout<<"\n a tabela é _\n";
 	}
 	// imprime a matriz com zero e a coordenada escolhida
 	
-	cout<<endl<<"digite as coordenadas \"y x\" ."<<endl;
-	cout<<"use valores negativos para sair do programa."<<endl;
-	cout << "coodenadas";
-	cin>>y>>x;
+
 	
-	while (x>=0 && y>=0){
-		matriz [y][x]=1;
-		for(y=0; y<altura;y+=1){
-			for(x=0;x<largura;x+=1){
-				cout <<matriz[y][x]<<" ";
-			}
-			cout <<endl;
-			cout <<"coordenadas";
-			cin>>y>>x;
-			
+	cout<<setw(6)<<0;
+	for (x=1;x<largura;x+=1){
+		cout<<setw(3)<<x;
+	cout<<endl;
+	cout<<" ";
+		
+	}
+	
+	for (x=0; x<3*largura;x+=1){
+		cout<<"_";
+		cout<<endl;
+	}
+	
+	for(y=0;y<altura;y+=1){
+		cout <<setw(2)<<y<<"|";
+		for(x=0;x<largura;x+=1){
+			cout<<setw(3)<<matriz[y][x];
+			cout<<endl;
 		}
 	}
 	
+
 }
