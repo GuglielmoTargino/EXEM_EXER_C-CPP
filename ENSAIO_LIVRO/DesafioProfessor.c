b6aa62b371183448ccd3e7aad1486a77668474be
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-15 16:37:46 -0300

    beta16h36-Feito comprimento stringer
    Concluído o medidor do comprimento da string

--- ENSAIO_LIVRO/DesafioProfessor.c
+++ ENSAIO_LIVRO/DesafioProfessor.c
@@ -1,13 +1,11 @@
 /*
 
-Exercício com manipulação de string
+Exercício resolução Sesafio Professor
 
 Aluno: Guglielmo Targino
-Data:28fev24
+Data:15mar24
 
-versão: v2
-
-
+versão: v0
 
 */
 
@@ -17,9 +15,21 @@ versão: v2
 void main(void){
 	
 	setlocale(LC_ALL,"Portuguese");
-	char s1[100],s2[100];
+	char s1[100];
+	int i,cx;
+	
 	printf("digite uma string\n");
 	gets(s1);
+	cx=strlen(s1);
+	
+	for(i=0;i<cx;i++){
+		printf("comprimento de s1 é =%c\n",s1[i]);
+		
+	}
+	
+	
+	
+	/*
 	printf("digite outra string\n");
 	gets(s2);
 	printf("comprimento de s1=%d, comprimento s2=%d\n",strlen(s1),strlen(s2));
@@ -31,4 +41,5 @@ void main(void){
 	printf(s1);
 	if(strchr("alo",'m')) printf("o está em alo\n");
 	if(strstr("olá aqui","olá")) printf("ola encontrado em olá aqui");	
+	*/
 }
