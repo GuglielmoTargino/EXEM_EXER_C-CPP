f634d672d2a6833ee701ad38d72890ad09e5b404
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-22 20:26:04 -0300

    cr-classe-main
    Criada a clsse main principal

---
+++ ENSAIO_LIVRO/TREINO-JAVA/FUNCIONARIO/src/Teste.java
@@ -0,0 +1,13 @@
+/*
+Exercício do livro JAVA
+Aluno: Guglielmo Targino.
+Data: 22mar24.
+Versão: v0.
+ */
+public class Teste {
+    public static void main(String[]args){
+        Funcionario func=new Funcionario();
+        
+    }
+    
+}
