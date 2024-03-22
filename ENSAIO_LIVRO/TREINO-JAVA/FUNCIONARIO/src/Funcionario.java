0fd4d4b14d9f23f8ae6d9e8bf09bac52d868a015
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-22 19:16:03 -0300

    fto-construtor
    Finalizado o metodo construtor.

---
+++ ENSAIO_LIVRO/TREINO-JAVA/FUNCIONARIO/src/Funcionario.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Guglielmo H T
+ */
+public class Funcionario {
+    private int matricula;
+    private String nome;
+    private String cargo;
+    private double salario;
+    
+    /* metodo construtor*/
+public Funcionario(int matricula, String nome, String cargo, double salario){
+    this.cargo=cargo;
+    this.matricula=matricula;
+    this.nome=nome;
+    this.salario=salario;   
+    
+}
+    
+}
