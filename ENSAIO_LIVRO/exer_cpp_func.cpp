834d994e29b295972b1b26f453d5050bfd042790
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-25 19:40:54 -0300

    ok_exer.
    Finalizado o exercío com linguagem CPP

--- ENSAIO_LIVRO/exer_cpp_func.cpp
+++ ENSAIO_LIVRO/exer_cpp_func.cpp
@@ -8,11 +8,47 @@ Versão: v0.
 */
 
 #include <iostream>
+#include <locale>
+#include <iomanip>
 
 using namespace std;
 
+
+	
+float salario(float sl){
+	
+		sl*=1.5;	
+		return sl;
+	}
+	
+float comissao(float cm){
+	cm+=1.0212333;
+	return cm;
+}
+
 int main(void){
-	cout<<"Helo Futute!";
+	setlocale(LC_ALL,"Portuguese");
+	
+	float cx,cs,total;
+	
+
+	cx=salario(10);
+	cs=comissao(8);
+	total=cx+cs;
+	
+	
+	//cout.setf(ios::fixed|ios::showpoint);
+	//cout.precision(2);
+	//cout<<"\n a largura com 2 é: "<<total;
+	
+	cout<<fixed<<setprecision(4)<<"\n"<<total;
+	
+ //std::cout << std::fixed << std::setprecision(4) <<"\n" <<total << std::endl;
+	
+	
+	
+	
+	
 }
 
 
