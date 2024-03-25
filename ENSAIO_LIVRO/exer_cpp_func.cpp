b94494875517f8cdb17eaa04d94c0798c4e62ad4
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-25 17:53:17 -0300

    exer_cpp
    Exercício com linguagem CPP.

---
+++ ENSAIO_LIVRO/exer_cpp_func.cpp
@@ -0,0 +1,18 @@
+/*
+exercício da apostila cpp
+
+Aluno Gulielmo Targino
+Data: 25/mar/24.
+Versão: v0.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	cout<<"Helo Futute!";
+}
+
+
