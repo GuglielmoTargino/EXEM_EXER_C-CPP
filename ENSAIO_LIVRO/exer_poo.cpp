d2db33d120219da456975079d4f3026fbcf42fe6
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-26 15:54:39 -0300

    ok_exer_poo
    Finalizado o exercicio poo em cpp.

--- ENSAIO_LIVRO/exer_poo.cpp
+++ ENSAIO_LIVRO/exer_poo.cpp
@@ -13,6 +13,36 @@ Versão: v0.
 
 using namespace std;
 
+class Carro{
+	private:
+	string marca;
+	string mod;
+	int ano;
+	
+	public:
+		//construtor
+		Carro(string m, string md, int a){
+			marca=m;
+			mod=md;
+			ano=a;
+		}
+		
+		// metodo para exibir
+		void ExibirCarro(){
+			cout<<"marca"<< marca<<endl;
+			cout<<"modelo"<<mod<<endl;
+			cout<<"ano"<<ano<<endl;
+			
+		}
+			
+};
+
+
 int main(void){
+	
+	//objeto de carro
+	Carro veiculo("fiat","brava",200);
+	
+	veiculo.ExibirCarro();
 	
 }
