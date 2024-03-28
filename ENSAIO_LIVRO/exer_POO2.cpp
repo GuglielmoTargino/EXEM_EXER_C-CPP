5064db336450ddbd4950e6192c4a973e8a86e820
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-28 15:03:19 -0300

    ok_teste_POO
    Finalizado o teste com POO

--- ENSAIO_LIVRO/exer_POO2.cpp
+++ ENSAIO_LIVRO/exer_POO2.cpp
@@ -5,4 +5,39 @@ Data: 28mar24.
 Versão: v0
 */
 
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Pessoa{
+	private:
+	 string name;
+	 int idade;
+	 
+	 
+	 public:
+		Pessoa(string nome, int dade){
+	 		name=nome;
+	 		idade=dade;	 		
+	 		
+		 }
+	 public:
+		void exibirPessoa(){
+			cout<<"nome é:\n"<<name<<endl;
+			cout<<"idade é:\n"<<idade<<endl;
+		};
+		
+	
+	
+};
+
+
+
+int main(void){
+	
+	Pessoa andrea("andrea",38);
+	andrea.exibirPessoa();
+	
+	
+	
+}
