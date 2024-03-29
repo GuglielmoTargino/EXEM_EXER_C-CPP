24ddb4c69fb0600f650e78335472545302269c5e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-29 20:57:02 -0300

    ok_exer_arrys_objeto
    Finalizado o estudo com arrays de objeto.

--- ENSAIO_LIVRO/TREINO-JAVA/FUNCIONARIO/src/Teste.java
+++ ENSAIO_LIVRO/TREINO-JAVA/FUNCIONARIO/src/Teste.java
@@ -13,18 +13,21 @@ public class Teste {
         Funcionario empregado[];
         empregado=new Funcionario[2];
         Funcionario func;
+   
         for(int indice=0; indice<empregado.length;indice++){
             func=new Funcionario();
-            func.setNome(JOptionPane.showInputDialog("Nome"+(indice+1)));
-            func.setCargo(JOptionPane.showInputDialog("Cargo"+(indice+1)));
+            func.setNome(JOptionPane.showInputDialog("Digite Nome"+(indice+1)));
+            func.setCargo(JOptionPane.showInputDialog("Digite Cargo"+(indice+1)));
+            func.setMatricula(Integer.parseInt(JOptionPane.showInputDialog("Digite Matricula"+(indice+1))));
             empregado[indice]=func;
+            
             
         }
         
         for(int indice=0;indice<empregado.length;indice++){
             func=empregado[indice];
-            JOptionPane.showMessageDialog(null, "Nome do empregado é:"+func.getNome());
-            JOptionPane.showMessageDialog(null, "Cargo do empregado é:"+func.getCargo());
+            JOptionPane.showMessageDialog(null, "Nome do empregado "+(indice+1)+" é "+func.getNome());
+            JOptionPane.showMessageDialog(null, "Cargo do empregado é:"+(indice+1)+"é"+func.getCargo());
         }
         
     }
