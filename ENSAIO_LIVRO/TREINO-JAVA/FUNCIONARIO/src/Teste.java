8870451e2dcbcecea288fc9d6982e223446a1af6
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-03-29 20:19:21 -0300

    exer_java_arr
    Exercício de arrays com java.

--- ENSAIO_LIVRO/TREINO-JAVA/FUNCIONARIO/src/Teste.java
+++ ENSAIO_LIVRO/TREINO-JAVA/FUNCIONARIO/src/Teste.java
@@ -9,21 +9,24 @@ Versão: v0.
  */
 public class Teste {
     public static void main(String[]args){
-        double saj;
-     
-        Funcionario func=new Funcionario();
-        func.setMatricula(67543);
-        func.setNome("Guga");
-        func.setSalario(1000);
-        saj=func.ajustarSalario(10);
         
+        Funcionario empregado[];
+        empregado=new Funcionario[2];
+        Funcionario func;
+        for(int indice=0; indice<empregado.length;indice++){
+            func=new Funcionario();
+            func.setNome(JOptionPane.showInputDialog("Nome"+(indice+1)));
+            func.setCargo(JOptionPane.showInputDialog("Cargo"+(indice+1)));
+            empregado[indice]=func;
+            
+        }
         
-        //System.out.println(func.getNome());
-        //JOptionPane.showMessageDialog(null,"Nome é :"+ func.getNome());
-        //JOptionPane.showMessageDialog(null,"a matricula é :"+func.getMatricula());
-        //JOptionPane.showMessageDialog(null,"Salario é:"+ saj);
-        System.out.println(saj);
+        for(int indice=0;indice<empregado.length;indice++){
+            func=empregado[indice];
+            JOptionPane.showMessageDialog(null, "Nome do empregado é:"+func.getNome());
+            JOptionPane.showMessageDialog(null, "Cargo do empregado é:"+func.getCargo());
+        }
         
     }
-    
+     
 }
