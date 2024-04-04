e1348db8d9937579d3af13095ccc0370df3c672d
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-04 16:31:50 -0300

    exer_testes
    Feito testes de comandos de c em c++.

--- ENSAIO_LIVRO/exer_matriz.cpp
+++ ENSAIO_LIVRO/exer_matriz.cpp
@@ -14,10 +14,26 @@ using namespace std;
 int main(){
 	setlocale(LC_ALL,"Portuguese");
 	
- int mtz[2][3];
-  	 mtz[0][0]={30};
+ int mtz[2][3]={{2,3,8},
+ 	 		    {1,6,7}
+				  
+};
+ int i,j,g;
+ 
+  for (i=0;i<2;i++){
+  	for(j=0;j<3;j++){
+  		
+  		//cout<<"matriz [0][0] é \n_"<<mtz[i][j]<<endl;
+  		printf("Matriz [%d][%d] é %d \n",i,j,mtz[i][j]);
+  		  		
+	  };
+  	 
+  };
+  
+   scanf("%d",&g);
+   	printf("vc digitou %d\n",g);
 	
-	cout<<"matriz [0][0] é "<<mtz[0][0];
+	
 	
 	return 0;
 }
