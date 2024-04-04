efc299978d5990e066ee4797e4b9b18a6ceeffe3
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-04 16:40:49 -0300

    exer_mtz
    Feito a migração de codigo de c para c++.

---
+++ ENSAIO_LIVRO/DesafioProfessor2.cpp
@@ -0,0 +1,129 @@
+/*
+
+Exercício resolução Sesafio Numerologia Professor
+
+Aluno: Guglielmo Targino
+Data:15mar24
+
+versão: v2, desenvolvido em linguagem de programação c++.
+
+*/
+
+#include <iostream>
+#include <cstring>
+#include <locale>
+
+
+using namespace std;
+
+int main(void){
+	
+	setlocale(LC_ALL,"Portuguese");
+	char s1[100],s2[100];
+	int i,cx,delta,ce,de,un,uni,dec,code,resu;
+	delta=0;
+	
+	printf("=============================================\n");
+	printf("PROGRAMA DESENVOLVIDO POR GUGLIELMO TARGINO.\n");
+	printf("ALUNO DO CURSO DE CIÊNCIA DA COMPUTAÇÃO\n");
+	printf("UNIVERSIDADE NOVE DE JULHO-UNINOVE\n");
+	printf("=============================================\n");
+	printf("SAIBA SEU NÚMERO DA SORTE- TÉCNICA EGÍPSIA\n");
+	printf("=============================================\n");
+	
+	printf("Digite seu nome_\n");
+	gets(s1);
+	cx=strlen(s1);
+	
+	for(i=0;i<cx;i++){
+		s2[i]=tolower(s1[i]);
+		
+		
+		switch(s2[i]){
+		case ' ':			
+			break;
+		
+		case 'a':
+		case 'j':
+		case 's':		
+			delta+=1;
+			break;
+						
+		case 'b':
+		case 'k':
+		case 't':			
+			delta+=2;
+			break;
+	
+		case 'c':
+		case 'l':
+		case 'u':			
+			delta+=3;
+			break;
+			
+		case 'd':
+		case 'm':
+		case 'v':			
+			delta+=4;
+			break;
+			
+		case 'e':
+		case 'n':
+		case 'w':			
+			delta+=5;			
+			break;
+			
+		case 'f':
+		case 'o':
+		case 'x':			
+			delta+=6;
+			break;
+			
+		case 'g':
+		case 'p':
+		case 'y':			
+			delta+=7;
+			break;
+			
+		case 'h':
+		case 'q':
+		case 'z':			
+			delta+=8;
+			break;
+			
+		case 'i':
+		case 'r':			
+			delta+=9;
+			break;
+		
+		default:
+			printf("ERRO. Favor não digitar números ou simbolos_\n");
+			i=cx;
+			delta=0;
+		}
+	
+	}
+	
+		ce=delta/100;	
+		de=(delta%100)/10;
+			
+		un=delta%10;
+		code=un+de+ce;
+		dec=code/10;
+		uni=code%10;
+		resu=dec+uni;
+		
+		if(delta==11 || delta ==22){
+			printf(" Seu número mestre da sorte é %d.\n",delta);
+		}else{
+			printf("Resultado para cáculo: %d\n ",delta);
+
+	printf("Seu número da sorte é %d.\n ",resu);
+			
+		}		
+	
+	system("pause");	
+	}
+	
+	
+
