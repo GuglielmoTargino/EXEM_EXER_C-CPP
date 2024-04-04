b8afba20ff27a918e2241c3bcb0a1c9614f30ee1
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-04 15:32:32 -0300

    exer_matriz
    Exercícios com matriz apenas em linguagem de programação c++.

---
+++ ENSAIO_LIVRO/exer_matriz.cpp
@@ -0,0 +1,20 @@
+/*
+Treino exercício com matriz bidimensional
+Aluno: Guglielmo Targino.
+Data: 04abr24.
+versão: v0
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	int mtz[][]={{2,3,8}, {1,6,7}};
+	
+	cout<<"matriz [0][0] é "<<mtz[0][0];
+	
+	return 0;
+}
