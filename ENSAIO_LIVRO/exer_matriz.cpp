4eacb82da5be337389121a9cccd1ed820c1cbca4
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-04 15:45:33 -0300

    exer_matriz
    testes apenas

--- ENSAIO_LIVRO/exer_matriz.cpp
+++ ENSAIO_LIVRO/exer_matriz.cpp
@@ -6,13 +6,16 @@ versão: v0
 */
 
 #include <iostream>
+#include <locale>
 
 using namespace std;
 
+
 int main(){
+	setlocale(LC_ALL,"Portuguese");
 	
-	
-	int mtz[][]={{2,3,8}, {1,6,7}};
+ int mtz[2][3];
+  	 mtz[0][0]={30};
 	
 	cout<<"matriz [0][0] é "<<mtz[0][0];
 	
