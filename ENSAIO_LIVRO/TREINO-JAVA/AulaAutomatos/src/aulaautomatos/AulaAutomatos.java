3930345fcf0df9e8dce01fd783a7ef827c0d10a1
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-09 11:25:06 -0300

    exer_automatos
    Exercíco de aula de automatos

---
+++ ENSAIO_LIVRO/TREINO-JAVA/AulaAutomatos/src/aulaautomatos/AulaAutomatos.java
@@ -0,0 +1,22 @@
+
+package aulaautomatos;
+
+/*
+Treino de aula de automatos.
+ @author Guglielmo H T.
+Data: 09abr24.
+Versão: v0.
+ */
+public class AulaAutomatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String palavra="testeguga020202";
+        
+        // TODO code application logic here
+    }
+    
+}
