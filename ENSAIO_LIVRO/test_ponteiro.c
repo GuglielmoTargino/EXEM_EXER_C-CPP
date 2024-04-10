24f10347b27f85572ee67629992ee26f7c86eb13
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-10 18:11:18 -0300

    trn_ponteiros
    Treino com ponteiros.

--- ENSAIO_LIVRO/test_ponteiro.c
+++ ENSAIO_LIVRO/test_ponteiro.c
@@ -1,12 +1,29 @@
+
+/*
+treino com ponteiros.
+Aluno: Guglielmo TARGINO.
+Data: 10abr24.
+Versão: v0
+
+*/
+
+
 #include <stdio.h>
 
 char *match(char c, char *s); /* modelo de prototipo*/
+
+char *match(char c, char *s){
+	while(c!=*s && *s) s++;
+	return (s);
+}
 
 
 void main (void){
 
 	char s[80], *p, ch;
+	printf("Digite uma string\n");
 	gets(s);
+	printf("Digite um caracter\n");
 	ch=getchar();
 	p=match(ch, s);
 	if(*p){
@@ -17,7 +34,4 @@ void main (void){
  	printf("nao encontrado %c em %s", ch, s);	
 }
 
-char *match(char c, char *s){
-	while(c!=*s && *s) s++;
-	return (s);
-}
+
