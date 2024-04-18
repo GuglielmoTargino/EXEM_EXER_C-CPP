c7afdffd8f0da2efa0d2cb7f9f13bb1c935a7ad5
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-17 23:22:07 -0300

    fim_trn_sizeof
    Finalizado o teste com a função sizeof().

--- ENSAIO_LIVRO/exer_lenght.cpp
+++ ENSAIO_LIVRO/exer_lenght.cpp
@@ -8,5 +8,24 @@ versao: vo
 */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+int main(){
+	
+   string ght="guglielmo targino";
+   char ch;
+   
+	int guga;
+	//.length() conta quantos elementos a variável tem.
+	int dara=ght.length();
+	//cout<<dara;
+
+	
+	//sizeof só conta os bytes do tipo da variável.
+	cout<<sizeof(ch);
+	
+	return 0;
+	
+}
--- ENSAIO_LIVRO/sizeof_teste.c
+++ ENSAIO_LIVRO/sizeof_teste.c
@@ -9,7 +9,8 @@ void main(void){
 	struct u ght;
 	printf("%d\n",sizeof(ght.i));
 	printf("%d\n",sizeof(ght.g));
-	printf("%d\n",sizeof(ght.f));// resultado é em byte.
+	printf("%d\n",sizeof(ght.f));
 	printf("%d",sizeof(ght));
+	// resultado mostra quantos bytes tem cada tipo de variável.
 	
 }
