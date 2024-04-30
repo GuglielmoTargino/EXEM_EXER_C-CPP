93c2c41d9428f3e1581745186d634ce897ea1cef
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-30 17:13:16 -0300

    ini_exerc_produto
    Montagem exercicio produto da apostila JAVA.

---
+++ ENSAIO_LIVRO/TREINO-JAVA/ExercicioProj2Produto/src/exercicioproj2produto/ExercicioProj2Produto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicioproj2produto;
+
+/**
+ *
+ * @author Guglielmo H T
+ */
+public class ExercicioProj2Produto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ENSAIO_LIVRO/TREINO-JAVA/ExercicioProj2Produto/src/produto/Produto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package produto;
+
+/**
+ *
+ * @author Guglielmo H T
+ */
+public class Produto {
+    
+}
