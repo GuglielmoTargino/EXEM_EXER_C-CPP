4b028b70f4a491f6ff906619e5e90f18e77c5a02
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-04-30 23:55:13 -0300

    cont_exer_java_prod
    Continuação do exercício classe produto. Feito metodo get/set id.

--- ENSAIO_LIVRO/TREINO-JAVA/ExercicioProj2Produto/src/exercicioproj2produto/ExercicioProj2Produto.java
+++ ENSAIO_LIVRO/TREINO-JAVA/ExercicioProj2Produto/src/exercicioproj2produto/ExercicioProj2Produto.java
@@ -1,20 +1,29 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+Exercicio do livro JAVA Evandro Teruel.
+Aluno: Guglielmo Targino.
+Data: 30abr24
+Versão: v0.
+*/
 package exercicioproj2produto;
+import javax.swing.JOptionPane;
+import produto.Produto;
 
-/**
- *
- * @author Guglielmo H T
- */
+
+
 public class ExercicioProj2Produto {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Produto livro;
+        livro =new Produto();
+        
+       
+        livro.setId(20);
+        
+        JOptionPane.showMessageDialog(null,"numero"+livro.getId());
+        
     }
     
 }
