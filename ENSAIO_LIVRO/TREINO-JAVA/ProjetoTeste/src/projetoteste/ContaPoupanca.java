0c694eaf7e49ff0494ba9d98e6999ac868a8303a
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-05-03 20:52:48 -0300

    fim_subclasse_contapoupanca
    Finalizada a construção da subclasse contapoupanca.

---
+++ ENSAIO_LIVRO/TREINO-JAVA/ProjetoTeste/src/projetoteste/ContaPoupanca.java
@@ -0,0 +1,27 @@
+
+package projetoteste;
+
+import java.util.Date;
+
+/**
+ Exercício do livro java pelo prof. Evandro Teruel
+ * @author Guglielmo H T
+ * Data: 03mai24
+ * Versão: v0.
+ */
+public class ContaPoupanca extends Conta{
+    private Date dataaniversario;
+    
+    //construtor parametrizado.
+    public ContaPoupanca(Date dataaniversario,String nrbanco,String nragencia, String nrconta,
+        String nmcliente,double saldo){
+        
+        super(nrbanco,nragencia,nrconta,nmcliente,saldo); 
+        this.dataaniversario=dataaniversario;        
+    }
+    //construtor não parametrizado.
+    public ContaPoupanca(){
+            
+        }
+    
+}
