314f7482b4df2822bfe969b850b9525bdf83d65e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-05-16 18:26:59 -0300

    fim_exer_somadois
    Finalizado o exercício soma dois

--- ENSAIO_LIVRO/exer_somadois.c
+++ ENSAIO_LIVRO/exer_somadois.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 
-int matriz[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-int vetor[10];
-int i,j,indice=0;
+
+int vetor[10]={1,2,3,4,5,6,7,8,9};
+int i,j;
 int alvo=5;
 
 
 int main(void) {
 
  
-    for(i=0;i<3;i++){
-      for(j=0;j<3;j++){
+   
+  
+  
+   for(i=0;i<10;i++){
+      for(j=0;j<10;j++){
     
       //transforma matriz em vetor
-      vetor[indice]=matriz[i][j];
-    
-       printf("valor vetor_matriz %d\n",vetor[indice]);
-    indice++;
+      if(vetor[i]+vetor[j]==alvo){
+      	printf("numero_5 no vetor\n");
+	  }else printf("numero_5 nao esta no vetor\n");
+	   
+	  }
     }
-  }
-   
-
+	  
   return 0;
 }
