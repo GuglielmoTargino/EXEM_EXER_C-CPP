4ecb56d104233bbb547ec4292dde671909730439
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-05-17 17:27:04 -0300

    exem_prof_luiscaparroz
    Outros exemplos do professor luis caparroz.

---
+++ ENSAIO_LIVRO/exer1_prof_luiscaparroz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int exercicio1(int vetor[10], int valor1, int valor2){
+	int encontrouValor1 = 0;
+	int encontrouValor2 = 0;
+	for(size_t indice = 0; indice < 10; indice++){
+		if(valor1 == vetor[indice]){
+			encontrouValor1 =  1;
+		}
+
+		if(valor2 == vetor[indice]){
+			encontrouValor2 = 1;
+		}
+	}
+
+	return encontrouValor1 && encontrouValor2;
+}
+
+int main(void) {
+  int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	if(exercicio1(vetor, 2, 8)) {
+		printf("Valores existem");
+	}
+	else {
+		printf("Não existe");
+	}
+  return 0;
+}
