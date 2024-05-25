f1ba71d611b070d4f15da861258a438e4b7b849a
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-05-25 12:16:29 -0300

    ini_classe_administrativo
    Iniciada a construção da classe administrativo.

---
+++ ENSAIO_LIVRO/TREINO-JAVA/ProjetoAbs/src/Administrativo.java
@@ -0,0 +1,19 @@
+
+
+/**exercício do livro JAVA prof. Evandro Teruel
+ * @author Guglielmo H 
+ * Data:25mai24
+ * Versão:v0
+ */
+public class Administrativo extends Funcionarios {
+    
+    //construtor da classe com atributos herdados de funcionario
+    public Administrativo(int id, String nome, String categoria,
+            double salario, double desconto){
+        super(id,nome,categoria,salario,desconto);
+        
+    }
+    
+    
+    
+}
