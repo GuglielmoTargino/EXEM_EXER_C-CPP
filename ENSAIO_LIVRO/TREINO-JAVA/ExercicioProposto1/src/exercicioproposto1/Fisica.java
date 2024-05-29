7f2a2611749d08dff6f1029cc36bf21219a5d0e9
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-05-29 00:09:29 -0300

    ini_classe_fisica
    Iniciada a classe fisica

---
+++ ENSAIO_LIVRO/TREINO-JAVA/ExercicioProposto1/src/exercicioproposto1/Fisica.java
@@ -0,0 +1,12 @@
+/**
+ *Exercicio proposto 1 do livro Java do prof. Evandro Teruel.
+ * @author Guglielmo H T
+ * Data:29mai24
+ * Versão:v0.
+ */
+package exercicioproposto1;
+
+
+public class Fisica {
+    
+}
