53a2958ea5595a81bf22be8a9335e514ee3c3734
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-05-28 23:44:20 -0300

    fim_metodo_pessoa
    Finalizada a classe Pessoa.

--- ENSAIO_LIVRO/TREINO-JAVA/ExercicioProposto1/src/exercicioproposto1/Pessoa.java
+++ ENSAIO_LIVRO/TREINO-JAVA/ExercicioProposto1/src/exercicioproposto1/Pessoa.java
@@ -7,13 +7,24 @@ package exercicioproposto1;
 
 public class Pessoa implements Validador {
     private String nome;
-    
-    public void escreverNome(){
-        
+            
+    public Pessoa(String nome){
+        this.nome=nome;
     }
-    @Override
-    public boolean validarDocumento(){
-        return true;
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
+
+    public String getNome() {
+        return nome;
+    }
+   public void escreverNome(){
+       System.out.println("Nome é"+this.nome);
+   }
+   @Override
+   public boolean validarDocumento(){
+       return true;
+   }
     
 }
