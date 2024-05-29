bca8b0c34596369ac52c0519bfbdf0434510efcb
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-05-28 22:41:08 -0300

    fim_inteface_validador
    Finalizada a interface validador de documentos.

---
+++ ENSAIO_LIVRO/TREINO-JAVA/ExercicioProposto1/src/exercicioproposto1/Validador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package exercicioproposto1;
+
+/**
+ *
+ * @author Guglielmo H T
+ */
+public interface Validador {
+    
+}
