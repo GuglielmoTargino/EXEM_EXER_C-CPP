94cb0bd347c43d0c7db489c29a9ca307f7c8c479
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-05-28 22:33:46 -0300

    ini_exer_prop1
    Iniciado o exercício proposto 1 do livro de java pelo prof. Evandro Teruel.

---
+++ ENSAIO_LIVRO/TREINO-JAVA/ExercicioProposto1/src/exercicioproposto1/ExercicioProposto1.java
@@ -0,0 +1,17 @@
+/**
+ *Exercicio proposto 1 do livro Java do prof. Evandro Teruel.
+ * @author Guglielmo H T
+ */
+package exercicioproposto1;
+
+
+public class ExercicioProposto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
