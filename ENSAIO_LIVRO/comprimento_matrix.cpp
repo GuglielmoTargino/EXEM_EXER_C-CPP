bb86f4bfc78135830ec3e0b19133082ccd961240
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-06-05 11:23:25 -0300

    fim_teste_comprimento_matriz
    Finalizado o comprimento da largura com matriz

--- ENSAIO_LIVRO/comprimento_matrix.cpp
+++ ENSAIO_LIVRO/comprimento_matrix.cpp
@@ -1,7 +1,7 @@
 /*
-Exercício com arrays da apostila cpp.
+Exercício de comprimento de uma matriz.
 Aluno:Guglielmo Targino.
-data: 13fev24.
+data: 05jun24.
 Versão: v0.
 
 */
@@ -10,53 +10,19 @@ Versão: v0.
 #include <iomanip>
 #include <locale>
 
-#define altura 10
+#define altura 5
 #define largura 10
 
 using namespace std;
+
+int tamanho;
 
 int main (void){
 	setlocale(LC_ALL,"Portuguese");
 	int x,y;
 	int matriz[altura][largura];
+	tamanho=sizeof(matriz)/sizeof(matriz[0]);
 	
-	for(y=0;y<altura;y+=1){
-		for(x=0;x<largura;x+=1){
-			matriz[y][x]=y*x;
-		
-		}
-	
-	}
-	// imprime a matriz com zero e a coordenada escolhida
-	
-	cout<<"\n a tabela é _\n";
-	
-	cout<<setw(6)<<0;
-	for (x=1;x<largura;x+=1){
-		cout<<setw(3)<<x;
-	//cout<<endl;
-	cout<<"";
-		
-	}
-	cout<<endl;
-	
-	for (x=0; x<3*largura;x+=1){
-		cout<<"_";
-		
-	}
-	
-	cout<<endl;
-	
-	for(y=0;y<altura;y+=1){
-		cout<<endl;
-		cout <<setw(2)<<y<<"|";
-		
-		for(x=0;x<largura;x+=1){
-			
-			cout<<setw(3)<<matriz[y][x];
-			//cout<<endl;
-		}
-	}
-	
+	cout<<tamanho;
 
 }
