5a641c80aa78f0765cef3a88459a67880824e16d
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-06-06 21:17:13 -0300

    fim_valor_compra
    Finalizado o exercício com valor de compra.

--- ENSAIO_LIVRO/bublesort.c
+++ ENSAIO_LIVRO/bublesort.c
@@ -1,7 +1,15 @@
+//Exercício com ordenador buble
+//Aluno: Guglielmo Targino.
+//Data:06jun24
+//Versão:v0.
+
 #include <stdio.h>
+#include <locale.h>
 
 int nt;
-int i;
+int i,vc;
+//,vc,soma;
+vc=60;
 void bubbleSort(int arr[], int n) {
     int i, j, temp;
     for (i = 0; i < n-1; i++) {
@@ -20,16 +28,36 @@ void bubbleSort(int arr[], int n) {
 void imprimirVetor(int arr[], int size) {
     for ( i = 0; i < size; i++) {
         printf("%d ", arr[i]);
-         printf("tamanho de size %d\n",size);
-        printf("o ultimo é %d\n",arr[size-1]);
+         
+        
         
     }
 
-    printf("o primeiro %d\n",arr[0]);
+    printf("\no primeiro é %d\n",arr[0]);
+    printf("o ultimo é %d\n",arr[size-1]);
     
+}
+void fazerCompra(int vetor[],int vc,int n){
+	int soma=0;
+	for(i=0;i<n-1;i++){
+		if(soma<vc){
+			soma+=vetor[i];
+			
+			if(soma>vc){
+				soma-=vetor[i];
+				soma+=vetor[0];
+				printf("valor total é %d \n",soma);
+		
+			}
+			
+			}	
+	}
+	
+	
 }
 
 int main() {
+	setlocale(LC_ALL,"Portuguese");
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr)/sizeof(arr[0]);
    
@@ -37,6 +65,7 @@ int main() {
     printf("Vetor ordenado: \n");
     imprimirVetor(arr, n);
     printf("Numero de trocas: %d\n", nt);
+    fazerCompra(arr,vc,n);
     return 0;
 }
 
