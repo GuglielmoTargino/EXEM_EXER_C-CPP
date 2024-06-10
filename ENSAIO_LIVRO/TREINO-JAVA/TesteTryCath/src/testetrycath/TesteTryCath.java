5a7579d87d8f04664e47a11944b7402c6c474d58
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-06-10 18:32:55 -0300

    fim_teste_try_catch
    Finalizado o exercício com try catch

--- ENSAIO_LIVRO/TREINO-JAVA/TesteTryCath/src/testetrycath/TesteTryCath.java
+++ ENSAIO_LIVRO/TREINO-JAVA/TesteTryCath/src/testetrycath/TesteTryCath.java
@@ -1,20 +1,20 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ Teste com exercício try catch do livro java Evandro Teruel
+Aluno:Guglielmo Targino.
+Data: 10jun24
  */
 package testetrycath;
 
-/**
- *
- * @author Guglielmo H T
- */
 public class TesteTryCath {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String[] lista=new String[3];
+        lista[0]="dara";
+        lista[1]="lic";
+        lista[2]="NIA";
+        System.out.println("nome é_"+lista[1]);
     }
     
 }
