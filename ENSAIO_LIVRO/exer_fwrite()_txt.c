120946aaffa5169176b510f9d89beb3df2780154
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-08-24 18:17:48 -0300

    adj_text
    ajuste de texto

--- ENSAIO_LIVRO/exer_fwrite()_txt.c
+++ ENSAIO_LIVRO/exer_fwrite()_txt.c
@@ -4,41 +4,32 @@
 #include <conio.h>
 
 typedef struct {
-    //int id;
+    int id;
     char name[20];
-    //float salary;
+    float salary;
 } Employee;
 
 int main() {
-	int y=0;
-	//	int i,y=0;
+
+	int i=0;
 	char x[10];
-	//float z=0;
-	
-	while(!y){
+	float z=0;
 		  
 		printf("digite um nome:");
-		gets(x);
+		gets(x);	
 	
-	
-		//printf("digite um numero:");
-		//scanf("%d",&i);
-	
+		printf("digite um numero:");
+		scanf("%d",&i);	
 		
-		//printf("digite um salario");
-		//scanf("%f",&z);
-		
-		
-		
-			
+		printf("digite um salario");
+		scanf("%f",&z);			
 	
 	    FILE *file;
 	    Employee emp;
 	    //strcpy(emp.name, "Camiseta");
-	    //emp.id=i;
-	    strcpy(emp.name,x);
-	    
-	    //emp.salary=z;
+	    emp.id=i;
+	    strcpy(emp.name,x);	    
+	    emp.salary=z;
 	    //Employee emp = {1,"guga", 55000.0f};
 	
 	    // Abre o arquivo para escrita em modo texto
@@ -46,24 +37,22 @@ int main() {
 	
 	    file = fopen("C:/Users/Guglielmo H T/Desktop/teste_php.txt","w");
 	    //file = fopen("employee2.txt","w");
+	    
 	    if (file == NULL) {
 	        printf("Erro ao abrir o arquivo.\n");
 	        return 1;
 	    }
 	
 	    // Escreve os dados da estrutura emp no arquivo em formato de texto
-	    //fprintf(file, "ID: %d", emp.id);
-	    fprintf(file, "Name: %s", emp.name);
-	    //fprintf(file, "Salary: %.2f", emp.salary);
+	    fprintf(file, "ID: %d %s %f ", emp.id, emp.name, emp.salary);
+	    //fprintf(file, "ID: %d\n", emp.id);
+	    //fprintf(file, "Name: %s\n", emp.name);
+	    //fprintf(file, "Salary: %.2f\n", emp.salary);
 	
 	    printf("Dados escritos com sucesso no arquivo de texto!\n");
-		printf("gigite 1 para sair 0 para continuar");
-		
-	    scanf("%d",&y);
+	
 	    // Fecha o arquivo
-	    fclose(file);  
-	    
-	}
+	    fclose(file); 	
 
     return 0;
 }
